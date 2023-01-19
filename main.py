0fb6711a86553d6fcf519a985b7408212acfd4dc
mb6ockatf <mb6ockatf@relay.firefox.com>
2023-01-20 00:30:06 +0300

    add: argument parsing

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
 #!/usr/bin/env python3
 """
 Script to select telegram chat history dump in json format
-
 Dump consists of chat's information (name, members, etc).
 chat["messages"] is usually the biggest value there.
 It is a list of all messages sent (except the ones which were deleted)
 Every message is a dictionary, too.
-
 Example message:
-
 {'date': '2029-06-33T05:33:11',
  'date_unixtime': '1455555555',
  'from': 'nickname',
@@ -19,14 +16,13 @@ Example message:
  'text_entities': [{'text': 'hi',
                     'type': 'plain'}],
  'type': 'message'}
-
 (all id's are broken there, all time mentioned is wrong dut to security reasons)
-
 It's convenient to use `pprint` module while parsing the data
-
-mb6ockatf, Thu 12 Jan 2023 06:16:30 PM MSK
+mb6ockatf, Fri 20 Jan 2023 12:27:28 AM MSK
 """
+
 import json
+from argparse import ArgumentParser
 from pprint import pprint
 
 
@@ -42,16 +38,20 @@ def print_data(message: dict):
     print("*" * 10)
 
 
-users = ["user2413266467418", "user34731274821"]
-with open("result.json", "r") as file:
-    contents = file.read()
-a = json.loads(contents)["messages"]
-for element in a:
-    try:
-        if element["from_id"] == "user1234567890":
+if __name__ == "__main__":
+    prog = "tgHistoryObserver"
+    desc = "Select data from Telegram chat history dump"
+    parser = ArgumentParser(prog, desc)
+    parser.add_argument("filename", type=str)
+    parser.add_argument("-u", "--from-user", action="append", type=str)
+    args = parser.parse_args()
+    with open(args.filename, "r") as file:
+        contents = file.read()
+    stock = json.loads(contents)["messages"]
+    for element in stock:
+        try:
+            if element["from_id"] in args.from_user:
+                print_data(element)
+        except KeyError:
             pprint(element)
-        elif element["from_id"] in users:
-            print_data(element)
-    except KeyError:
-        pprint(element)
 
